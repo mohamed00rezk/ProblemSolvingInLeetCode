8a849dc8714690944b2094d901bcc91033ad7aa6
MRezk <Mohammed.Rezk@naseej.com>
2023-08-03 15:12:55 +0300

    update

--- ProblemSolvingInLeetCode/Program.cs
+++ ProblemSolvingInLeetCode/Program.cs
@@ -5,13 +5,13 @@ Console.WriteLine("Hello, World!");
 
 
 /// ValidParentheses
-//   String s = "[([]])"; // "(([]){})"; // "{[]})("; // "(("; // "()";
-//   ValidParentheses v = new ValidParentheses();
-//   var res = v.IsValid(s);
-//   Console.WriteLine("res: " +  res);
+String s = "([])"; // "(([]){})"; // "{[]})("; // "(("; // "()";
+ValidParentheses v = new ValidParentheses();
+var res = v.IsValid(s);
+Console.WriteLine("res: " + res);
 
-// TwoSum
-TwoSum s = new TwoSum();
-int[] nums = new[] { 1, 3, 4, 2 };
-int[] res = s.TwoSum_1(nums, 6);
-Console.WriteLine("res: " + res.First() + " , " + res.Last());
+//// TwoSum
+//TwoSum s = new TwoSum();
+//int[] nums = new[] { 1, 3, 4, 2 };
+//int[] res = s.TwoSum_1(nums, 6);
+//Console.WriteLine("res: " + res.First() + " , " + res.Last());
--- ProblemSolvingInLeetCode/solutions/ValidParentheses.cs
+++ ProblemSolvingInLeetCode/solutions/ValidParentheses.cs
@@ -13,10 +13,108 @@ namespace ProblemSolvingInLeetCode.solutions
         private static string PATTERNOFOPEN = "({[";
         private static string PATTERNOFCLOSE = ")}]";
 
-        private static string PATTERN_1 = "()";
-        private static string PATTERN_2 = "{}";
-        private static string PATTERN_3 = "[]";
+        private static Dictionary<char, char> PATTERN_MAP = new Dictionary<char, char>() {{ '(', ')' } , { '{', '}' } , { '[', ']' } };
+
+
         public bool IsValid(string input)
+        {
+            
+            List<Char> chars = input.ToList();
+
+            if (chars.Count % 2 == 1)
+            {
+                return false;
+            }
+            if (checkIfALLInputIsOpenOrClose(chars))
+            {
+                return false;
+            }
+
+            Dictionary<int, char> map = new Dictionary<int, char>();
+            for (int i = 0; i < chars.Count; i++)
+            {
+                map.Add(i , chars[i]);
+            }
+            Dictionary<int, char> outputCharMap = new Dictionary<int, char>();
+
+            foreach (KeyValuePair<int ,char> ch in map)
+            {
+                if (!PATTERN.Contains(ch.Value))
+                {
+                    return false;
+                }
+                outputCharMap.Add(outputCharMap.Count(), ch.Value);     
+                if (PATTERNOFCLOSE.Contains(ch.Value))
+                {
+                    char openCh = PATTERN_MAP.FirstOrDefault(o => o.Value.Equals(ch.Value)).Key;                  
+                    if (!outputCharMap.ContainsValue(openCh))
+                    {
+                        return false;
+                    }
+                    
+                    int orderShift = getOrderShift(outputCharMap, openCh);
+
+                    if (!isOrderTrue(outputCharMap, openCh, ch.Value, orderShift))
+                    {
+                        return false;
+                    }
+
+                    outputCharMap = remove(outputCharMap, ch.Value, orderShift);
+
+                }
+
+            }
+            if (outputCharMap.Count > 0)
+            {
+                return false;
+            }
+            return true;
+        }
+        private int getOrderShift(Dictionary<int, char> outputCharMap, char openCh)
+        {
+            int openChIndex = outputCharMap.LastOrDefault(o => o.Value.Equals(openCh)).Key;
+
+            Dictionary<int, char> tempArray = new Dictionary<int, char>();
+            tempArray =  outputCharMap;
+
+            var tempArrayX = tempArray.SkipWhile(o => o.Key < openChIndex);
+
+            int shiftCount = tempArrayX.Where(o => o.Value != openCh && PATTERNOFOPEN.Contains(o.Value)).Count();
+
+            return shiftCount != 0 ? (shiftCount * 2) + 1 : 1;
+        }
+        private bool isOrderTrue(Dictionary<int, char> outputCharMap, char openCh, char closeCh, int orderShift)
+        {
+            int openChIndex  = outputCharMap.LastOrDefault(o => o.Value.Equals(openCh)).Key;   // 1  // 2
+            int closeChIndex = outputCharMap.LastOrDefault(o => o.Value.Equals(closeCh)).Key; ; // 4  // 3
+            if (closeChIndex == openChIndex + orderShift)
+            {
+                return true;
+            }
+            return false;
+        }
+        private Dictionary<int, char> remove(Dictionary<int, char> outputCharMap, char ch, int orderShift)
+        {
+            int closeChIndex = outputCharMap.LastOrDefault(o => o.Value.Equals(ch)).Key;
+            outputCharMap.Remove(closeChIndex);
+            outputCharMap.Remove(closeChIndex - orderShift);
+
+            return outputCharMap;
+        }
+        private bool checkIfALLInputIsOpenOrClose(List<char> charArray)
+        {
+            return charArray.Where(ch => PATTERNOFOPEN.Contains(ch)).Count() == charArray.Count ||
+                   charArray.Where(ch => PATTERNOFCLOSE.Contains(ch)).Count() == charArray.Count;
+        }
+
+
+
+
+        // solution with o(n2)
+        private static string PATTERN_1_2 = "()";
+        private static string PATTERN_2_2 = "{}";
+        private static string PATTERN_3_2 = "[]";
+        public bool IsValid_2(string input)
         {
             List<Char> outputCharArray = new List<Char>();
             var charArray = input.ToList();
@@ -24,7 +122,7 @@ namespace ProblemSolvingInLeetCode.solutions
             {
                 return false;
             }
-            if (checkIfALLInputIsOpenOrClose(charArray))
+            if (checkIfALLInputIsOpenOrClose_1(charArray))
             {
                 return false;
             }
@@ -67,17 +165,20 @@ namespace ProblemSolvingInLeetCode.solutions
             return outputCharArray;
         }
 
-        private int getOrderShift(List<Char> outputCharArray, char openCh )
+        private int getOrderShift( List<Char> outputCharArray, char openCh )
         {
             List<Char> tempArray  = new List<char>();
             tempArray.AddRange(outputCharArray);
+
             int openChIndex = tempArray.FindLastIndex(o => o.Equals(openCh));
+
             tempArray.RemoveRange(0 , openChIndex);
+
             int shiftCount = tempArray.Where(o => o!= openCh && PATTERNOFOPEN.Contains(o)).Count();
             return shiftCount!= 0 ? (shiftCount * 2) + 1 : 1;
         }
 
-        private bool checkIfALLInputIsOpenOrClose(List<char> charArray)
+        private bool checkIfALLInputIsOpenOrClose_1(List<char> charArray)
         {
            return charArray.Where(ch => PATTERNOFOPEN.Contains(ch)).Count() == charArray.Count ||
                   charArray.Where(ch => PATTERNOFCLOSE.Contains(ch)).Count() == charArray.Count ;
@@ -91,16 +192,16 @@ namespace ProblemSolvingInLeetCode.solutions
         private Char getTheOpenOfTheCloseChar(char ch)
         {
             Char x ;
-            if (PATTERN_1.Contains(ch))
+            if (PATTERN_1_2.Contains(ch))
             {
-                x =  PATTERN_1.ToArray()[0];
+                x =  PATTERN_1_2.ToArray()[0];
             }
-            else if (PATTERN_2.Contains(ch))
+            else if (PATTERN_2_2.Contains(ch))
             {
-                x = PATTERN_2.ToArray()[0];
+                x = PATTERN_2_2.ToArray()[0];
             } 
             else 
-                x = PATTERN_3.ToArray()[0];
+                x = PATTERN_3_2.ToArray()[0];
 
             return x;
         }
