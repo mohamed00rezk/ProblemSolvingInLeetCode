9c5b54aed93a8670fb3c60dc564d9284ac8d48ef
MRezk <Mohammed.Rezk@naseej.com>
2023-08-03 15:26:34 +0300

    update

---
+++ ProblemSolvingInLeetCode/solutions/MergeTwoSortedLists.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProblemSolvingInLeetCode.solutions
+{
+    public class MergeTwoSortedLists
+    {
+        public LinkedListNode MergeTwoLists(LinkedListNode list1, LinkedListNode list2)
+        {
+
+        }
+    }
+}
